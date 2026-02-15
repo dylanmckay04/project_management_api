e26d93cae0c58503aafae50abf3f53b0df208056
Dylan <dylanmckay8939@gmail.com>
2026-02-15 01:43:23 -0500

    Remove unused imports

--- frontend/src/pages/Projects.tsx
+++ frontend/src/pages/Projects.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import { useQuery, useQueryClient } from '@tanstack/react-query'
 import api from '../api/axios'
 import { Link } from 'react-router-dom'
